dd5cb432008f047d750858fb3e1d89251e2ab4da
unknown <SYSTEM@store02.(none)>
2012-06-14 17:50:02 +0000

    Build version 0.3

---
+++ superpass2012/chat/postMessageConstants.js
@@ -0,0 +1,10 @@
+
+window._pmConstants = {
+	MAXIMIZE_IFRAME   : 'CSPM_maximize_iframe_height',
+	MINIMIZE_IFRAME   : 'CSPM_minimize_iframe_height',
+	SAY_CLOSE_POPUP   : 'CSPM_close_popup_please',
+	SAY_OPEN_ROOM     : 'CSPM_say_open_room',
+	CLOSE_APPLICATION : 'CSPM_close_application',
+	TOGGLE_APPLICATION : 'CSPM_toggle_application',
+	CLOSE_POPUP_MESSAGE : 'CSPM_close_popup_please'
+};
